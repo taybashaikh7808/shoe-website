e9158e279796f60fbf315ef4b7718594f9a76982
taybaa <tayba3@gmail.com>
2025-02-11 16:26:40 +0530

    added all the required files

---
+++ src/Navigations/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CiHeart ,CiUser , AiOutlineShoppingCart} from "react-icons/ci";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <input />
+      </div>
+      <div>
+        <a href="">
+          <CiHeart />
+          <AiOutlineShoppingCart />
+          <CiUser />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
