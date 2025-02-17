75ae617f63b66ac322fcb91ac86ecdbb2268cf95
taybaa <shaikhtayba3@gmail.com>
2025-02-17 23:59:55 +0530

    CHNGED COLOR CHECK BOX BLACK AND WHITE

--- src/Colors/Colors.jsx
+++ src/Colors/Colors.jsx
@@ -1,5 +1,5 @@
-import Input from '../components/Input'
-import React from 'react'
+import Input from '../components/Input';
+import React from 'react';
 
 const Colors = ({ handleCheckbox }) => {
   return (
@@ -7,11 +7,41 @@ const Colors = ({ handleCheckbox }) => {
       <div className="font-sans text-[20px] flex justify-center items-center mt-7">
         Colors
       </div>
-      <Input onChange={handleCheckbox} value="black" name="color" title="Black" />
-      <Input onChange={handleCheckbox} value="white" name="color" title="White" />
-      <Input onChange={handleCheckbox} value="blue" name="color" title="Blue" />
-      <Input onChange={handleCheckbox} value="brown" name="color" title="Brown" />
-      <Input onChange={handleCheckbox} value="green" name="color" title="Green" />
+      <Input 
+        onChange={handleCheckbox} 
+        value="black" 
+        name="color" 
+        title="Black" 
+        color="#000000" 
+      />
+      <Input 
+        onChange={handleCheckbox} 
+        value="white" 
+        name="color" 
+        title="White" 
+        color="#ffffff" 
+      />
+      <Input 
+        onChange={handleCheckbox} 
+        value="blue" 
+        name="color" 
+        title="Blue" 
+        color="#0000ff" 
+      />
+      <Input 
+        onChange={handleCheckbox} 
+        value="brown" 
+        name="color" 
+        title="Brown" 
+        color="#8B4513" 
+      />
+      <Input 
+        onChange={handleCheckbox} 
+        value="green" 
+        name="color" 
+        title="Green" 
+        color="#008000" 
+      />
     </>
   );
 };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,21 +2,18 @@ import React from "react";
 
 const Input = ({ handleCheckbox, value, title, color, name }) => {
   return (
-    <>
-      <div className=" pl-15 mt-4 space-y-2 ml-20">
-        <label className="flex items-center space-x-2">
-          <input
-            onChange={handleCheckbox}
-            type="checkbox"
-            className="mr-2"
-            name={name}
-            value={value}
-            style={{ backgroundColor: color }}
-          />
-          {title}
-        </label>
-      </div>
-    </>
+    <div className="pl-15 mt-4 space-y-2 ml-20">
+      <label className="flex items-center space-x-2">
+        <input
+          onChange={handleCheckbox}
+          type="checkbox"
+          name={name}
+          value={value}
+          className={`mr-2 accent-${value}`}  // Dynamic color classes based on value
+        />
+        {title}
+      </label>
+    </div>
   );
 };
 
